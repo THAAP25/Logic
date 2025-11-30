c03b29ca7213899815fc2d92ffe7327209f28c29
THAAP25 <tokoinotaq@gmail.com>
2025-11-30 23:41:10 +0100

    Glucose upload

---
+++ glucose-main/simp/depend.mk
@@ -0,0 +1,40 @@
+/home/dimon/glucose-main/simp/Main.o /home/dimon/glucose-main/simp/Main.or /home/dimon/glucose-main/simp/Main.od /home/dimon/glucose-main/simp/Main.op: /home/dimon/glucose-main/simp/Main.cc \
+ /home/dimon/glucose-main/simp/../utils/System.h \
+ /home/dimon/glucose-main/simp/../mtl/IntTypes.h \
+ /home/dimon/glucose-main/simp/../utils/ParseUtils.h \
+ /home/dimon/glucose-main/simp/../utils/Options.h \
+ /home/dimon/glucose-main/simp/../mtl/Vec.h \
+ /home/dimon/glucose-main/simp/../mtl/XAlloc.h \
+ /home/dimon/glucose-main/simp/../core/Dimacs.h \
+ /home/dimon/glucose-main/simp/../core/SolverTypes.h \
+ /home/dimon/glucose-main/simp/../mtl/Alg.h \
+ /home/dimon/glucose-main/simp/../mtl/Map.h \
+ /home/dimon/glucose-main/simp/../mtl/Alloc.h \
+ /home/dimon/glucose-main/simp/../simp/SimpSolver.h \
+ /home/dimon/glucose-main/simp/../mtl/Queue.h \
+ /home/dimon/glucose-main/simp/../core/Solver.h \
+ /home/dimon/glucose-main/simp/../mtl/Heap.h \
+ /home/dimon/glucose-main/simp/../core/BoundedQueue.h \
+ /home/dimon/glucose-main/simp/../core/Constants.h \
+ /home/dimon/glucose-main/simp/../mtl/Clone.h \
+ /home/dimon/glucose-main/simp/../core/SolverStats.h
+/home/dimon/glucose-main/simp/SimpSolver.o /home/dimon/glucose-main/simp/SimpSolver.or /home/dimon/glucose-main/simp/SimpSolver.od /home/dimon/glucose-main/simp/SimpSolver.op: /home/dimon/glucose-main/simp/SimpSolver.cc \
+ /home/dimon/glucose-main/simp/../mtl/Sort.h \
+ /home/dimon/glucose-main/simp/../mtl/Vec.h \
+ /home/dimon/glucose-main/simp/../mtl/IntTypes.h \
+ /home/dimon/glucose-main/simp/../mtl/XAlloc.h \
+ /home/dimon/glucose-main/simp/../simp/SimpSolver.h \
+ /home/dimon/glucose-main/simp/../mtl/Queue.h \
+ /home/dimon/glucose-main/simp/../core/Solver.h \
+ /home/dimon/glucose-main/simp/../mtl/Heap.h \
+ /home/dimon/glucose-main/simp/../mtl/Alg.h \
+ /home/dimon/glucose-main/simp/../utils/Options.h \
+ /home/dimon/glucose-main/simp/../utils/ParseUtils.h \
+ /home/dimon/glucose-main/simp/../core/SolverTypes.h \
+ /home/dimon/glucose-main/simp/../mtl/Map.h \
+ /home/dimon/glucose-main/simp/../mtl/Alloc.h \
+ /home/dimon/glucose-main/simp/../core/BoundedQueue.h \
+ /home/dimon/glucose-main/simp/../core/Constants.h \
+ /home/dimon/glucose-main/simp/../mtl/Clone.h \
+ /home/dimon/glucose-main/simp/../core/SolverStats.h \
+ /home/dimon/glucose-main/simp/../utils/System.h
